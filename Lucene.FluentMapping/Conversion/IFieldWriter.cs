d8b12fc379ccd879b0d7303bf790c9b4e3231924
Matt Kelly <mattk@mattkelly.me.uk>
2013-05-26 16:40:36 -0100

    tidy up

--- Lucene.FluentMapping/Conversion/IFieldWriter.cs
+++ Lucene.FluentMapping/Conversion/IFieldWriter.cs
@@ -4,6 +4,8 @@ namespace Lucene.FluentMapping.Conversion
 {
     public interface IFieldWriter<T>
     {
+        // TODO re-use IFieldable impls - they are expensive to create!
+
         IFieldable GetField(T instance);
     }
 }
--- Lucene.FluentMapping/IDocumentMapper.cs
+++
@@ -1,8 +0,0 @@
-namespace Lucene.FluentMapping
-{
-    public interface IDocumentMapper<T>
-    {
-        // TODO consider method which sets fields on an existing Document
-        // lucene docs say (said?) re-using document & field instance improves performance
-    }
-}
