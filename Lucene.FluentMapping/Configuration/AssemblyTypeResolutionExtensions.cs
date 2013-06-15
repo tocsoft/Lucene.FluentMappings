0d21d6e876b3b3216af059f7a47ffe618ea91d01
Scott Williams <tocsoft@gmail.com>
2013-06-15 12:21:22 +0100

    added SafeGetTypes Assembly extension method
    this will prevent types with missing dependencies from causing GetTypesImplementing from throwing

--- Lucene.FluentMapping/Configuration/AssemblyTypeResolutionExtensions.cs
+++ Lucene.FluentMapping/Configuration/AssemblyTypeResolutionExtensions.cs
@@ -9,8 +9,27 @@ namespace Lucene.FluentMapping.Configuration
     {
         public static IEnumerable<Type> GetTypesImplementing(this IEnumerable<Assembly> @this, Type interfaceType)
         {
-            return @this.SelectMany(a => a.GetTypes())
+            return @this.SelectMany(a => a.SafeGetTypes())
                         .Where(interfaceType.IsAssignableFrom);
+        }
+
+        /// <summary>
+        /// Safely the get types accessible from the assembly.
+        /// </summary>
+        /// <remarks>This will ignore any types where dependencies fail to load</remarks>
+        /// <param name="this">The this.</param>
+        /// <returns></returns>
+        public static IEnumerable<Type> SafeGetTypes(this Assembly @this)
+        {
+            try
+            {
+                return @this.GetTypes();
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                //these is a list for types that where successfully loaded from the assembly
+                return ex.Types;
+            }
         }
     }
 }
